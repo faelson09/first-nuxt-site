483a0f547d30e85dcd1d27fd382ecc6641b4f0b4
Faelson Nunes <122487090+faelson09@users.noreply.github.com>
2025-02-26 11:47:08 +0100

    testando heades da page home

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-   <section class="relative w-screen flex items-center justify-center h-[calc(100vh-3.5rem)]">
+   <section class="relative w-screen flex items-center justify-center h-[calc(100vh-3.5rem)] bg-blue-400">
       <div class="w-[calc(100%-12px)] max-w-[1200px] flex gap-5">
          <div class="flex-1 flex flex-col justify-center">
-            <h1 class="text-5xl font-bold mb-5">Ola! Eu sou <span class="relative before:h-2 before:w-full before:content-['ola_as'] before:absolute before:bg-blue-500 before:bottom-0 before:text-transparent before:overflow-hidden">Faelson</span> <span>Nunes</span>.</h1>
+            <h1 class="text-5xl font-bold mb-5">Ola! Eu sou <span class="relative before:h-2 before:w-full before:content-['Faelson'] before:absolute before:bg-blue-500 before:bottom-0 before:text-white before:overflow-hidden before:left-0 before:pb-3 before:flex before:items-end before:duration-700 before:hover:h-full">Faelson</span> <span>Nunes</span>.</h1>
             <p class="text-base">Fala, dev_! Eu sou Faelson09, um simples nerd aficionado por programação, tecnologia e tudo que envolve o mundo digital. Desde cedo, mergulhei no universo do código e, ao longo do tempo, fui aprimorando minhas habilidades em diversas linguagens e frameworks.</p>
          </div>
          <div class="flex-1 flex flex-col justify-center h-full">
